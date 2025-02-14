123e5d4c45ab4c8c86e321d59b5fc5c396906093
truonglamdev <truonglamdev070303@gmail.com>
2025-02-14 23:23:01 +0700

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/qua-valentine/",
+  base: "/valentine/",
   plugins: [react()],
 });
